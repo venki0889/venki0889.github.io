4c04bd27487c771d54577aef2dfc5dfe9e2aefa7
venki0889 <37668525+venki0889@users.noreply.github.com>
2018-12-28 16:09:16 -0600

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/nike.jpg') {
+      myImage.setAttribute ('src','images/nike1.jpg');
+    } else {
+      myImage.setAttribute ('src','images/nike.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Nike Donation, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Nike Donation, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
